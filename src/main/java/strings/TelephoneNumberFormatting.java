1e2c7a122927b210a72c12b2c6f026a409f5332d
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2019-03-24 12:17:56 +0530

    fixed the bug

--- src/main/java/strings/TelephoneNumberFormatting.java
+++ src/main/java/strings/TelephoneNumberFormatting.java
@@ -10,7 +10,7 @@ public class TelephoneNumberFormatting {
         if (len == 2) {
             return sb.append(telNumber).toString();
         }
-        while (i + 2 <= len  && len - i != 4 ) {
+        while (i + 2 < len  && len - i != 4 ) {
             sb.append(telNumber.charAt(i));
             sb.append(telNumber.charAt(i + 1));
             sb.append(telNumber.charAt(i + 2));
@@ -40,6 +40,16 @@ public class TelephoneNumberFormatting {
     }
 
     public static void main(String[] args) {
+        /*if s = "123", result "123"
+        ** s = "12345", result = "123-45"
+        ** s = "1234", result = "12-34"
+        ** s = "123456", result = "123-456"
+        ** s = "1234567", result = "123-45-67"
+         */
         System.out.println(formatTelNumber("123"));
+        System.out.println(formatTelNumber("12345"));
+        System.out.println(formatTelNumber("1234"));
+        System.out.println(formatTelNumber("123456"));
+        System.out.println(formatTelNumber("1234567"));
     }
 }
