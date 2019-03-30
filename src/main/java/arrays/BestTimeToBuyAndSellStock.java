329da7ef329b2b962abf40db86e6ada2cba8d2b3
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2019-03-30 12:32:41 +0530

    Buy and Sell stock

--- src/main/java/arrays/BestTimeToBuyAndSellStock.java
+++ src/main/java/arrays/BestTimeToBuyAndSellStock.java
@@ -26,9 +26,9 @@ public class BestTimeToBuyAndSellStock {
 
         int minSeenSoFar = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i - 1] > prices[i]) {
-                minSeenSoFar = Integer.min(minSeenSoFar, prices[i]);
-            }
+
+            minSeenSoFar = Integer.min(minSeenSoFar, prices[i]);
+
             maxProfit = Integer.max(maxProfit, prices[i] - minSeenSoFar);
 
         }
