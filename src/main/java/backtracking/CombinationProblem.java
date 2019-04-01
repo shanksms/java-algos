c0787ea7ca9fe711a10d9e9cddcdf70c09ba48bc
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2019-04-01 15:42:23 +0530

    Combination problem

---
+++ src/main/java/backtracking/CombinationProblem.java
@@ -0,0 +1,41 @@
+package backtracking;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+class CombinationProblem {
+    List<List<Integer>> output = new LinkedList<>();
+    int n;
+    int k;
+
+    public void backtrack(int first, LinkedList<Integer> curr) {
+        // if the combination is done
+        if (curr.size() == k) {
+            output.add(new LinkedList(curr));
+            return;
+        }
+
+        for (int i = first; i < n + 1; ++i) {
+            // add i into the current combination
+            curr.add(i);
+            // use next integers to complete the combination
+            backtrack(i + 1, curr);
+            // backtrack
+            curr.removeLast();
+        }
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
+        this.k = k;
+        backtrack(1, new LinkedList<Integer>());
+        return output;
+    }
+
+    public static void main(String[] args) {
+        CombinationProblem obj = new CombinationProblem();
+        obj.combine(4, 2);
+        System.out.println(obj.output);
+    }
+}
