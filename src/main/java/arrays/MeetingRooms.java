816c7bdec44821c422a4031aafa20899d4b4f244
unknown <sriv.srivastava@gmail.com>
2019-04-12 22:42:24 +0530

    Meeting rooms solution

---
+++ src/main/java/arrays/MeetingRooms.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+ * determine if a person could attend all meetings.
+ *
+ * Example 1:
+ *
+ * Input: [[0,30],[5,10],[15,20]]
+ * Output: false
+ * Example 2:
+ *
+ * Input: [[7,10],[2,4]]
+ * Output: true
+ */
+
+public class MeetingRooms {
+    public boolean canAttendMeetings(Interval[] intervals) {
+
+        Arrays.sort(intervals, Comparator.comparingInt(e -> e.start));
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
