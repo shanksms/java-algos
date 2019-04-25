f7c2a61b35eccdc7b24d8733ca4e7bfd047b31d8
shanks <sriv.srivastava@gmail.com>
2019-04-25 07:11:24 +0530

    Catalan number using DP

---
+++ src/main/java/dp/NumberOfBST.java
@@ -0,0 +1,22 @@
+package dp;
+
+public class NumberOfBST {
+
+  public static int numberOfBSTsGivenN(int n) {
+    int [] result = new int[n + 1];
+    result[0] = 1;
+    result[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
+        result[i] += result[j] * result[i - j -1];
+      }
+    }
+
+    return result[n];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(numberOfBSTsGivenN(4));
+  }
+
+}
