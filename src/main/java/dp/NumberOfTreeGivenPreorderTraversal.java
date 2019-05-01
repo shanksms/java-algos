2f301b375cb45b5e9dbc3dcd79897dde9f477962
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2019-05-01 20:47:48 +0530

    dp solution of number of binary tree possible

---
+++ src/main/java/dp/NumberOfTreeGivenPreorderTraversal.java
@@ -0,0 +1,23 @@
+package dp;
+
+public class NumberOfTreeGivenPreorderTraversal {
+
+    public int numberOfBinaryTree(int n) {
+        int [] result = new int[n + 1];
+        result[0] = 1;
+        result[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                result[i] += result[j] * result[i - j -1];
+            }
+        }
+
+        return result[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new NumberOfTreeGivenPreorderTraversal().numberOfBinaryTree(4));
+    }
+
+}
