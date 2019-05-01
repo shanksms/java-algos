b0ce235ee22409c6d03d540adb906e13650422bd
shanksms <sriv.srivastava@gmail.com>
2019-05-01 12:11:40 +0530

    shortest distance to a character code

---
+++ src/main/java/strings/ShortestDistanceToCharacter.java
@@ -0,0 +1,38 @@
+package strings;
+
+/**
+ * Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+ *
+ * Example 1:
+ *
+ * Input: S = "loveleetcode", C = 'e'
+ * Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+ *
+ *
+ * Note:
+ *
+ * S string length is in [1, 10000].
+ * C is a single character, and guaranteed to be in string S.
+ * All letters in S and C are lowercase.
+ */
+public class ShortestDistanceToCharacter {
+
+  public int  [] solution(String input, char ch) {
+
+    int [] result = new int[input.length()];
+    int position = -input.length();
+    //first pass
+    for (int i = 0; i < input.length(); i++) {
+      if (input.charAt(i) == ch) position = i;
+      result[i] = i - position;
+    }
+    //second pass
+    for (int i = input.length() - 1; i >= 0; i--) {
+      if (input.charAt(i) == ch) position = i;
+      result[i] = Math.min(result[i], Math.abs(i - position));
+    }
+
+
+    return result;
+  }
+}
