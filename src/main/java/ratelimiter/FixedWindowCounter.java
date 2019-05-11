501e32f5789067d1a9cd8deb75552ebb9d465f49
shanksms <sriv.srivastava@gmail.com>
2019-05-11 23:36:31 +0530

    Rate limiting algorithms

---
+++ src/main/java/ratelimiter/FixedWindowCounter.java
@@ -0,0 +1,28 @@
+package ratelimiter;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FixedWindowCounter extends RateLimiter {
+
+  // TODO: Clean up stale entries
+  private final ConcurrentMap<Long, AtomicInteger> windows = new ConcurrentHashMap<>();
+
+  protected FixedWindowCounter(int maxRequestPerSec) {
+    super(maxRequestPerSec);
+  }
+
+  @Override
+  boolean allow() {
+    long windowKey = System.currentTimeMillis() / 1000 * 1000;
+    windows.putIfAbsent(windowKey, new AtomicInteger(0));
+    return windows.get(windowKey).incrementAndGet() <= maxRequestPerSec;
+  }
+
+  /*public static void main(String[] args) {
+    System.out.println(System.currentTimeMillis() / 1000 * 1000);
+    System.out.println(System.currentTimeMillis());
+
+  }*/
+}
---
+++ src/main/java/ratelimiter/RateLimiter.java
@@ -0,0 +1,12 @@
+package ratelimiter;
+
+public abstract class RateLimiter {
+
+  protected final int maxRequestPerSec;
+
+  protected RateLimiter(int maxRequestPerSec) {
+    this.maxRequestPerSec = maxRequestPerSec;
+  }
+
+  abstract boolean allow();
+}
