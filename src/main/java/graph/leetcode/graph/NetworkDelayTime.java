1dc3d0e43eab9833e11852b0a1dd1dbc51de0b93
shanksms <sriv.srivastava@gmail.com>
2019-06-22 19:51:11 +0530

    check if a binary tree is height balanced

---
+++ src/main/java/graph/leetcode/graph/NetworkDelayTime.java
@@ -0,0 +1,18 @@
+package graph.leetcode.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/network-delay-time/discuss/210698/Real-Djikstrabfs-Java-Concise-very-easy-to-understand
+ *
+ */
+/*public class NetworkDelayTime {
+
+  public int networkDelayTime(int[][] times, int N, int K) {
+
+
+  }
+}*/
