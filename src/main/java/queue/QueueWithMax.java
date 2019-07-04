ad2f1be375d5b3eaa9fddd9e9007a6a36c84a699
shanksms <sriv.srivastava@gmail.com>
2019-07-04 14:08:00 +0530

    implemented Queue with max api

---
+++ src/main/java/queue/QueueWithMax.java
@@ -0,0 +1,51 @@
+package queue;
+
+
+import com.google.common.base.Preconditions;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class QueueWithMax  <T extends Comparable<T>> {
+
+  private LinkedList<T> queue;
+  private Deque<T> maxCandidates;
+
+  public QueueWithMax(LinkedList<T> queue, Deque<T> maxCandidates) {
+    Preconditions.checkNotNull(queue, "queue must be Not null");
+    Preconditions.checkNotNull(maxCandidates, "dequeue must  be not null");
+    this.queue = queue;
+    this.maxCandidates = maxCandidates;
+  }
+
+  public void enqueue(T element) {
+    Preconditions.checkNotNull(element, "Can not add null element");
+    queue.add(element);
+    while (!maxCandidates.isEmpty()) {
+      if (maxCandidates.getLast().compareTo(element) >= 0) {
+        break;
+      }
+      maxCandidates.removeLast();
+    }
+    maxCandidates.addLast(element);
+
+  }
+
+  public T dequeue() {
+    if (queue.isEmpty()) {
+      throw new NoSuchElementException("No element in the queue");
+    }
+    T element = queue.remove();
+    if (element == maxCandidates.getFirst()) maxCandidates.removeFirst();
+
+    return element;
+  }
+
+  public T max() {
+    if (maxCandidates.isEmpty()) throw new NoSuchElementException();
+    return maxCandidates.getFirst();
+  }
+
+
+
+}
---
+++ src/test/java/queue/QueueWithMaxTest.java
@@ -0,0 +1,39 @@
+package queue;
+
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueueWithMaxTest {
+  private QueueWithMax<Integer> queue;
+
+  @Before
+  public void setUp() throws Exception {
+    queue = new QueueWithMax<Integer>(new LinkedList<>(), new ArrayDeque<>());
+  }
+
+  @Test
+  public void enqueue() {
+  }
+
+  @Test
+  public void dequeue() {
+  }
+
+  @Test
+  public void max() {
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.dequeue();
+
+    Integer expectedMax = 3;
+    MatcherAssert.assertThat(queue.max(), CoreMatchers.equalTo(expectedMax));
+
+  }
+}
