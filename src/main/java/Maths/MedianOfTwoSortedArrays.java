466855011ee46708626d77242a6cc95156ed7b76
shanksms <sriv.srivastava@gmail.com>
2019-10-12 21:40:53 +0530

    corrected median array problem

--- src/main/java/Maths/MedianOfTwoSortedArrays.java
+++ src/main/java/Maths/MedianOfTwoSortedArrays.java
@@ -6,46 +6,39 @@ package Maths;
  */
 public class MedianOfTwoSortedArrays {
 
-  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-    int x = nums1.length;
-    int y = nums2.length;
-    if (y > x) findMedianSortedArrays(nums2, nums1);
-
-    int low = 0; int high = x;
-
-
-    while (low <= high) {
-
-      int partitionX = (low + high) / 2;
-      int partitionY = (x + y + 1) / 2 - partitionX;
-
-      int maxLeftX = partitionX == 0 ? Integer.MIN_VALUE : nums1[partitionX - 1];
-      int minRightX = partitionX == x ? Integer.MAX_VALUE : nums1[partitionX];
-
-      int maxLeftY = partitionY == 0 ? Integer.MIN_VALUE : nums2[partitionY - 1];
-      int minRightY = partitionY == y ? Integer.MAX_VALUE : nums2[partitionY];
-
-
-      if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-        if ((x + y) % 2 == 0) {
-          return ((double) Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
-        } else {
-          return (double) Math.max(maxLeftX, maxLeftY);
-        }
-
-      } else if (maxLeftX > minRightY) {
-        high = partitionX - 1;
-
-      } else {
-        low = partitionX + 1;
-      }
-
+  public double findMedianSortedArrays(int[] A, int[] B) {
+    int m = A.length;
+    int n = B.length;
+    if (m > n) { // to ensure m<=n
+      int[] temp = A; A = B; B = temp;
+      int tmp = m; m = n; n = tmp;
     }
+    int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+    while (iMin <= iMax) {
+      int i = (iMin + iMax) / 2;
+      int j = halfLen - i;
+      if (i < iMax && B[j-1] > A[i]){
+        iMin = i + 1; // i is too small
+      }
+      else if (i > iMin && A[i-1] > B[j]) {
+        iMax = i - 1; // i is too big
+      }
+      else { // i is perfect
+        int maxLeft = 0;
+        if (i == 0) { maxLeft = B[j-1]; }
+        else if (j == 0) { maxLeft = A[i-1]; }
+        else { maxLeft = Math.max(A[i-1], B[j-1]); }
+        if ( (m + n) % 2 == 1 ) { return maxLeft; }
 
+        int minRight = 0;
+        if (i == m) { minRight = B[j]; }
+        else if (j == n) { minRight = A[i]; }
+        else { minRight = Math.min(B[j], A[i]); }
 
-    throw new IllegalArgumentException("Arrays are not sorted");
+        return (maxLeft + minRight) / 2.0;
+      }
+    }
+    return 0.0;
   }
-
 
 }
