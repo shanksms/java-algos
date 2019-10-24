51c7e4e4ff764f322ebb5c9363e422ea33f89ead
shanksms <sriv.srivastava@gmail.com>
2019-10-24 08:41:58 +0530

    sqrt using binary search

---
+++ src/main/java/searching/Sqrt.java
@@ -0,0 +1,38 @@
+package searching;
+
+/**
+ * Implement int sqrt(int x).
+ *
+ * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+ *
+ * Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+ *
+ * Example 1:
+ *
+ * Input: 4
+ * Output: 2
+ * Example 2:
+ *
+ * Input: 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since
+ *              the decimal part is truncated, 2 is returned.
+ */
+public class Sqrt {
+  public int mySqrt(int x) {
+    if (x < 2) return x;
+
+    long num;
+    int pivot, left = 2, right = x / 2;
+    while (left < right) {
+      pivot = left + (right - left) / 2;
+      num = (long)pivot * pivot;
+      if (num > x) right = pivot;
+      else if (num < x) left = pivot + 1;
+      else return pivot;
+    }
+
+    return left;
+  }
+
+}
